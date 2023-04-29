cfa0bfd731bb5155ac9c1c07e1da852cae46b7e1
Juan Robledo <heaxenz@Rikos-PC>
2023-04-29 16:22:02 -0700

    update to js colors work

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ function randomRGB(){
 const letters = document.querySelectorAll('.letters');
 const intervalId = setInterval(function () {
   for(let letter of letters) {
-    letters.style.color = randomRGB();
+    letter.style.color = randomRGB();
   }
 }, 1000);
